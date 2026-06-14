2a24e45f1f36a36dc091d62d08f72a1c5939e41c
ZHANG ZION <zhixianzhang52@gmail.com>
2026-06-14 21:34:58 +0800

    Day3 Complete

---
+++ 01_verilog_basics/02_combinational/src/full_adder.v
@@ -0,0 +1,8 @@
+module full_adder(
+    input a, b, cin,
+    output cout,sum
+);
+    assign sum = a ^ b ^ cin;
+    assign cout = (a & b) | (a & cin) | (b & cin);
+
+endmodule
---
+++ 01_verilog_basics/02_combinational/tb/mux2to1_tb.v
@@ -0,0 +1,31 @@
+`timescale 1ns/1ps
+
+module mux2to1_tb;
+
+reg a, b, sel;
+wire y;
+
+mux2to1 uut (
+    .a(a),
+    .b(b),
+    .sel(sel),
+    .y(y)
+);
+
+initial begin
+    $dumpfile("waveforms/mux2to1.vcd");
+    $dumpvars(0, mux2to1_tb);
+
+    a = 0; b = 0; sel = 0; #10;
+    a = 0; b = 0; sel = 1; #10;
+    a = 1; b = 0; sel = 0; #10;
+    a = 1; b = 0; sel = 1; #10;
+    a = 0; b = 1; sel = 0; #10;
+    a = 0; b = 1; sel = 1; #10;
+    a = 1; b = 1; sel = 0; #10;
+    a = 1; b = 1; sel = 1; #10;
+
+    $finish;
+end
+
+endmodule
